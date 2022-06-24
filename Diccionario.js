09e7f0b2d586d527e51269c48e5c51ff8099d4eb
Julio Villanueva <wolfri2002@gmail.com>
2022-06-23 21:50:40 -0500

    Cambios en del Diccionario

--- Diccionario.js
+++ Diccionario.js
@@ -4,6 +4,6 @@ class Diccionario{
 	static total_barcos=5;
 	static barco=" O ";
 	static barco_destruido=" X ";
-	static turno_inicial=2; // 0 = CPU ; 1 = Jugador ; 2 aleatorio
+	static turno_inicial=0; // 0 = CPU ; 1 = Jugador ; 2 aleatorio
 
 }
